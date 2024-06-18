a23b157e11d05c1bc11f3b281e6cb9b8d138149b
Rodolfo <rortimoteo@gmail.com>
2024-06-18 18:28:01 -0300

    changes to show avatar icon

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -22,7 +22,7 @@ export function Profile(){
   const [passwordOld, setPasswordOld] = useState();
   const [passwordNew, setPasswordNew] = useState();
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}files/${user.avatar}` : avatarPlaceholder
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
   const [avatar, setAvatar] = useState(avatarUrl);
   const [avatarFile, setAvatarFile] = useState(null);
 
